08a5f0078a426e7916827d459c310be9b9f43ee1
Dinar <19yakudza99@gmail.com>
2025-08-12 20:14:59 +0500

    TestBase and Specs were improved

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,19 +1,10 @@
-import helpers.CustomApiListener;
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeAll;
-
-import static io.restassured.RestAssured.given;
 
 public class TestBase {
     @BeforeAll
     public static void setUp() {
         RestAssured.baseURI = "https://reqres.in";
         RestAssured.basePath = "/api";
-        RestAssured.requestSpecification = given()
-                .filter(CustomApiListener.withCustomTemplates())
-                .contentType(ContentType.JSON)
-                .header("x-api-key", "reqres-free-v1")
-                .log().all();
     }
 }
--- src/test/java/specs/Specs.java
+++ src/test/java/specs/Specs.java
@@ -1,12 +1,23 @@
 package specs;
 
 import helpers.CustomApiListener;
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
 public class Specs {
+    public static RequestSpecification requestSpec() {
+        return new RequestSpecBuilder()
+                .addFilter(CustomApiListener.withCustomTemplates())
+                .setContentType(ContentType.JSON)
+                .addHeader("x-api-key", "reqres-free-v1")
+                .log(LogDetail.ALL)
+                .build();
+    }
+
     public static ResponseSpecification responseSpec(int statusCode) {
         return new ResponseSpecBuilder()
                 .expectStatusCode(statusCode)
